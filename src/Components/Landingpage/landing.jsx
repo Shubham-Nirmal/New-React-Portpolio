4fd64f9fe294a80fe4210f46c8bd4d26c14086f5
shubham-Nirmal <shubhamnirmal2612@gmail.com>
2025-08-11 13:12:51 +0530

    Updated portfolio with latest changes

--- src/Components/Landingpage/landing.jsx
+++ src/Components/Landingpage/landing.jsx
@@ -13,12 +13,12 @@ const Landing = () => {
           A passionate Full Stack Developer with hands-on experience in building responsive and dynamic web and mobile applications.<br /><br />
           <strong>I specialize in:</strong><br />
           <strong>Frontend:</strong> HTML, CSS, JavaScript, React.js<br />
-          <strong>Backend & Database:</strong> Node.js, MySQL, MongoDB<br /><br />
+          <strong>Backend & Database:</strong> Node.js, MySQL, MongoDB , Express.js, Python<br /><br />
           I love creating clean, scalable solutions and turning ideas into real-world applications that deliver great user experiences.
         </p>
         <div className="landing-buttons">
           <a href="#contact" className="btn">Contact Me</a>
-          <a href="/resume.pdf" className="btn btn-outline" download>Download Resume</a>
+          <a href="/resume.pdf" className="btn btn-outline" download>Download Resume</a> 
         </div>
         <div className="social-icons">
           <a href="https://github.com/Shubham-Nirmal" target="_blank" rel="noreferrer"><FaGithub /></a>
